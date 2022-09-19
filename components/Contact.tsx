c7e302d24aa52c4dc95f865d1676e7529696922d
ferasahmad <firastaf878@gmail.com>
2022-09-19 01:21:20 -0400

    💄 UI: made contact section black

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -11,7 +11,7 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(black, transparent);
+  background: black;
 `;
 
 export default Contact;
