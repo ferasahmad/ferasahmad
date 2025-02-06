d08f18782650ea431cf87755b5277a656e6d138a
Feras Ahmad <firastaf878@gmail.com>
2025-02-06 01:25:04 -0500

    fix: Minor formatting adjustment in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import SectionContainer from "./SectionContainer";
 
 const Hero: NextPage = () => {
   return (
-    <SectionContainer>
+    <SectionContainer >
       <h1 className="text-center text-7xl tracking-[10px] lg:text-[70px] lg:text-nowrap sm:text-[20px] font-bold">
         FERAS AHMAD
       </h1>
