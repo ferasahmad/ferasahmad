9193b96b4e37b1cf46f4291a4864f49957d356a0
Feras Ahmad <firastaf878@gmail.com>
2025-02-06 01:24:38 -0500

    feat: Add personal bio and profile image to About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
 import SectionContainer from "./SectionContainer";
 
 export default function About() {
   return (
-    <SectionContainer className="bg-gray-500">
-      <h2>About</h2>
+    <SectionContainer className="justify-between lg:flex-row flex-col">
+      <Image
+        src="/images/me.svg"
+        height={300}
+        width={300}
+        alt="Profile picture"
+      />
+      <p className="lg:max-w-[400px] text-justify font-semibold text-2xl lg:text-xl">
+        Hey there! I&apos;m Feras, a Software Engineer based in New Jersey. When
+        I&apos;m not coding, I enjoy taking photos, attending concerts, gaming,
+        and baking (though I&apos;m not great at it...)
+      </p>
     </SectionContainer>
   );
 }
