6210e548b6e70db2d89811dd7f8453c92fdbb51f
Feras Ahmad <firastaf878@gmail.com>
2025-02-20 01:45:55 -0500

    feat: Add responsive title display for Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,12 @@ const Hero: NextPage = () => {
   return (
     <SectionContainer>
       <div className="flex-1">
-        <TextHoverEffect text="FERAS AHMAD" />
+        <div className="hidden md:block">
+          <TextHoverEffect text="FERAS AHMAD" />
+        </div>
+        <div className="md:hidden">
+          <h1 className="text-4xl font-bold">FERAS AHMAD</h1>
+        </div>
       </div>
     </SectionContainer>
   );
