19dac0cefc46f224d71369a898f0adf1e42c306e
Feras Ahmad <firastaf878@gmail.com>
2025-02-20 00:49:30 -0500

    change: Update About section text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,9 +11,9 @@ export default function About() {
         alt="Profile picture"
       />
       <p className="lg:max-w-[400px] text-justify font-semibold text-2xl lg:text-xl">
-        Hey there! I&apos;m Feras, a Software Engineer based in New Jersey. When
-        I&apos;m not coding, I enjoy taking photos, attending concerts, gaming,
-        and baking (though I&apos;m not great at it...)
+        I&apos;m Feras, a Software Engineer based in New Jersey. I build web and
+        mobile applications, and outside of work, I enjoy photography, concerts,
+        gaming, and changing the font of my website a hundred times.
       </p>
     </SectionContainer>
   );
