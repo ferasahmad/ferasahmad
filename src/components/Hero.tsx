d0b83e13b2e249db5488662238cb40d8c9b40f94
Feras Ahmad <firastaf878@gmail.com>
2025-02-20 01:55:24 -0500

    refactor: Reduce mobile Hero section title font size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero: NextPage = () => {
         <TextHoverEffect text="FERAS AHMAD" />
       </div>
       <div className="md:hidden">
-        <h1 className="text-6xl text-center font-bold">FERAS AHMAD</h1>
+        <h1 className="text-4xl text-center font-bold">FERAS AHMAD</h1>
       </div>
     </SectionContainer>
   );
