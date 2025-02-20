099ba3fe9e100ce40caba0c1c3bbcdd745d3bf1f
Feras Ahmad <firastaf878@gmail.com>
2025-02-20 01:40:21 -0500

    refactor: Apply text hover effect to Hero section title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,13 @@
 import { NextPage } from "next";
 import SectionContainer from "./SectionContainer";
+import { TextHoverEffect } from "./ui/text-hover-effect";
 
 const Hero: NextPage = () => {
   return (
-    <SectionContainer >
-      <h1 className="text-center text-7xl tracking-[10px] lg:text-[70px] lg:text-nowrap sm:text-[20px] font-bold">
-        FERAS AHMAD
-      </h1>
+    <SectionContainer>
+      <div className="flex-1">
+        <TextHoverEffect text="FERAS AHMAD" />
+      </div>
     </SectionContainer>
   );
 };
