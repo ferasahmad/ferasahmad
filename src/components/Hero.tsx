80d6d27671652a2badf427e995ccad2cc0519448
Feras Ahmad <firastaf878@gmail.com>
2025-04-11 22:17:58 -0400

    remove animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,13 @@
 import { NextPage } from "next";
 import SectionContainer from "./SectionContainer";
-import { TextHoverEffect } from "./ui/text-hover-effect";
 
 const Hero: NextPage = () => {
   return (
     <SectionContainer>
-      <div className="hidden flex-1 md:block">
-        <TextHoverEffect text="FERAS AHMAD" />
-      </div>
-      <div className="md:hidden">
-        <h1 className="text-4xl text-center font-bold">FERAS AHMAD</h1>
+      <div>
+        <h1 className="sm:text-[80px] text-4xl text-center font-bold">
+          FERAS AHMAD
+        </h1>
       </div>
     </SectionContainer>
   );
