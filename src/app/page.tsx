21792670fe5aeac17e3395b47601d356302f1b9f
Feras Ahmad <firastaf878@gmail.com>
2025-05-09 23:09:18 -0400

    refactor: Comment out About and Hero components in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import About from "@/components/About";
 import Contact from "@/components/Contact";
 // import Experience from "@/components/Experience";
-import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
