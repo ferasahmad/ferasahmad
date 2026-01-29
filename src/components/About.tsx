6e868eec31657d574b3cc56d187dc531bed71342
Feras Ahmad <firastaf878@gmail.com>
2026-01-29 11:48:41 -0500

    restore hero and added about me section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import Image from "next/image";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="flex h-[100vh] bg-[var(--bg-light)] p-8">
+      <div className="flex flex-1 flex-col justify-center p-8 md:p-12">
+        <p className="text-4xl text-black text-left mb-4">ABOUT ME</p>
+        <p className="text-2xl text-black mb-4">
+          Hi I’m Feras and I like to build software. I’ve worked on fintech
+          apps, content creator tools, and AI-driven products, but what I care
+          about most is making complex ideas feel simple through thoughtful
+          design and engineering.
+        </p>
+        <p className="text-2xl text-black">
+          When I'm not coding, you can find me doing some photography, hiking,
+          snowboarding, and sometimes, just sometimes, reading.
+        </p>
+      </div>
+      <div className="hidden lg:flex flex-1 items-center ">
+        <Image
+          src="/images/about-section/main.png"
+          alt=""
+          width={800}
+          height={800}
+          className=""
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <div className="flex h-[100vh]">
-      <div className="flex flex-1 flex-col items-center justify-center bg-[var(--bg-primary)] p-8 md:p-12">
+      <div className="flex flex-1 flex-col items-center justify-center bg-[var(--bg-primary)] p-8 md:p-0">
         <div className="relative">
           <div className="relative inline-block">
             <div
@@ -30,24 +30,18 @@ export default function Hero() {
               <Image
                 src="/images/hero-section/ferasahmad.png"
                 alt="Feras Ahmad"
-                width={200}
-                height={300}
+                width={300}
+                height={600}
                 className="relative z-10 object-contain"
                 priority
               />
             </div>
           </div>
         </div>
-        <div className="flex text-center w-[400px] items-center justify-between">
+        <div className="flex text-center w-[300px] items-center justify-between">
           <h1 className="text-lg text-black">FERAS AHMAD</h1>
           <p className="text-lg text-black">SOFTWARE ENGINEER</p>
         </div>
-        {/* <div className="text-center items-center justify-between">
-          <h1 className="text-sm text-black">CURRENTLY UNAVAILABLE.</h1>
-          <p className="text-sm text-black">
-            SENDING POSITIVE ENERGY YOUR WAY.
-          </p>
-        </div> */}
       </div>
 
       <div className="relative hidden flex-1 md:block">
