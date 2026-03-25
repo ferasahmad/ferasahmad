0e3bc1bf0e01d68a1a2773c19dace0f7ca5f3f0f
Feras Ahmad <firastaf878@gmail.com>
2026-03-25 15:14:56 -0400

    reformat

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,27 +6,27 @@ export default function About() {
     <section className="flex justify-center bg-[var(--bg-light)] p-8 md:p-12 lg:items-center lg:h-[100vh] lg:p-16 lg:px-32 text-black">
       <div className="flex w-full flex-col gap-10 lg:flex-row lg:items-center">
         <div className="flex flex-1 flex-col justify-center">
-        <p className="text-5xl text-black text-left mb-10">ABOUT ME</p>
-        <p className="text-2xl text-black mb-4">
-          Hi I’m Feras and I like to build software. I’ve worked on fintech
-          apps, content creator tools, and AI-driven products, but what I care
-          about most is making complex ideas feel simple through thoughtful
-          design and engineering.
-        </p>
-        <p className="text-2xl text-black">
-          When I'm not coding, you can find me doing some photography, hiking,
-          snowboarding, and sometimes, just sometimes... reading.
-        </p>
-      </div>
-      <div className="hidden lg:flex flex-1 items-center justify-end">
-        <Image
-          src="/images/about-section/main.png"
-          alt=""
-          width={600}
-          height={600}
-          className=""
-        />
-      </div>
+          <p className="text-5xl text-black text-left mb-10">ABOUT ME</p>
+          <p className="text-2xl text-black mb-4">
+            Hi I’m Feras and I like to build software. I’ve worked on fintech
+            apps, content creator tools, and AI-driven products, but what I care
+            about most is making complex ideas feel simple through thoughtful
+            design and engineering.
+          </p>
+          <p className="text-2xl text-black">
+            When I'm not coding, you can find me doing some photography, hiking,
+            snowboarding, and sometimes, just sometimes, reading.
+          </p>
+        </div>
+        <div className="hidden lg:flex flex-1 items-center justify-end">
+          <Image
+            src="/images/about-section/main.png"
+            alt=""
+            width={600}
+            height={600}
+            className=""
+          />
+        </div>
       </div>
     </section>
   );
