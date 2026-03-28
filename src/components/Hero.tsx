c7ec1a566ab8ec9a0353345f0bbd9b34f87e8899
Feras Ahmad <firastaf878@gmail.com>
2026-03-28 16:04:50 -0400

    replace images

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
             />
             <div className="block lg:hidden">
               <Image
-                src="/images/hero-section/ferasahmad.png"
+                src="/images/hero-section/ferasahmad.webp"
                 alt="Feras Ahmad"
                 width={300}
                 height={400}
@@ -28,7 +28,7 @@ export default function Hero() {
             </div>
             <div className="hidden lg:block">
               <Image
-                src="/images/hero-section/ferasahmad.png"
+                src="/images/hero-section/ferasahmad.webp"
                 alt="Feras Ahmad"
                 width={450}
                 height={600}
